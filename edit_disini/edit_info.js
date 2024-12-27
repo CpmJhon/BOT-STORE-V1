04cd32deb783bd7fe1f1bd9de73605b6037f5c17
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-12-28 06:08:31 +0700

    edit_info.js

---
+++ edit_disini/edit_info.js
@@ -0,0 +1,11 @@
+const { modul } = require('../module');
+const edit_info = `Di Bawah`// ga usah di otak atik
+
+const urlpepe = `https://b.top4top.io/p_2683roc9o5.jpg`// url/link logo
+
+const namaowner = `♕𝐌𝐓𝐒 𝐒𝐓𝐎𝐑𝐄♕`// nama owner
+const noowner = `6289513452028`// nomor owner
+
+const packname = `ayam`// nama pack sticker
+const author = `kokok`// nama author stiker
+module.exports = { edit_info,urlpepe,namaowner,noowner,packname,author }
