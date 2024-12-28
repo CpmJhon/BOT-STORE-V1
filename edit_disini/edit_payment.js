ee4bd145080a8649736aba4aef3a2949940adb41
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-12-28 10:01:04 +0700

    edit_payment.js

---
+++ edit_disini/edit_payment.js
@@ -0,0 +1,23 @@
+const { modul } = require('../module');
+const edit_payment = `Di Bawah`
+
+//EDIT PAYMENT GOPAY DI BAWAH
+const QRgopay = `https://telegra.ph/file/b976c4edda2afc771a7f0.jpg`
+const NOgopay = `085894955362`
+
+//EDIT PAYMENT DANA DI BAWAH
+const QRdana = `https://telegra.ph/file/b976c4edda2afc771a7f0.jpg`
+const NOdana = `085591916436`
+
+//EDIT PAYMENT OVO DI BAWAH
+const QRovo = `https://telegra.ph/file/b976c4edda2afc771a7f0.jpg`
+const NOovo = `085894955362`
+
+//EDIT PAYMENT SHOPEE DI BAWAH
+const QRshopee = `https://telegra.ph/file/b976c4edda2afc771a7f0.jpg`
+const NOshopee = `085894955362`
+
+//EDIT ALL PAYMENT DI BAWAH
+const QRallpay = `https://telegra.ph/file/b976c4edda2afc771a7f0.jpg`
+
+module.exports = { edit_payment,QRgopay,NOgopay,QRdana,NOdana,QRovo,NOovo,QRshopee,NOshopee,QRallpay }
